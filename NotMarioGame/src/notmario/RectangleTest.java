0605a1f5d6c864025106d19da7c7c9c47db5dab1
George Lasagna <36421467+georgelasagna@users.noreply.github.com>
2018-05-02 15:58:41 -0400

    Update RectangleTest.java

--- NotMarioGame/src/notmario/RectangleTest.java
+++ NotMarioGame/src/notmario/RectangleTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class RectangleTest {
+class RectangleTest implements ApplicationConstants {
 
 	@Test
 	void testInvalidIntersection() {
@@ -20,4 +20,18 @@ class RectangleTest {
 		assertTrue(r.isInside(c, 1));
 	}
 
+	@Test
+	void testNotEdge() {
+		Rectangle r = new Rectangle(0, 0, 100, 100, 0, 0, 0);
+		Character c = new Character(0, 100, 5, 1, 1, 1, null);
+		assertFalse(r.playerEdge(c, 1));
+	}
+
+/*  TODO: Improve test case!
+	void testEdge() {
+		Rectangle r = new Rectangle(2.5006f, -65, 70, 25, 0, 0, 0);
+		Character c = new Character(0, -0.375f, 1, 4, 1, 1, null);
+		assertTrue(r.playerEdge(c, 1));
+	}
+*/
 }
